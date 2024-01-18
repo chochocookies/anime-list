62655c0be50909fffd5927c9465b4d8733acce01
Eko Aryanto <ekoaryanto11@gmail.com>
2024-01-18 22:28:48 +0700

    add auth login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,12 @@
 import AnimeList from "@/components/AnimeList"
 import Header from "@/components/AnimeList/Header"
-import { getAnimeResponse } from "./libs/api-libs"
+import { getAnimeResponse, getNestedAnimeResponse, reproduce } from "../libs/api-libs"
 
 
 const Page = async () => {
-
   const topAnime = await getAnimeResponse("top/anime", "limit=8")
+  let recommendedAnime = await getNestedAnimeResponse("recommendations/anime", "entry")
+  recommendedAnime = reproduce(recommendedAnime, 4)
 
   return (  
     <>
@@ -13,6 +14,13 @@ const Page = async () => {
       <section>
       <Header title="Paling Populer" linkTitle="Lihat Semua" href="/populer"/>
       <AnimeList api={topAnime}/>
+
+      </section>
+
+      {/* anime rekomendasi */}
+      <section>
+      <Header title="Rekomendasi"/>
+      <AnimeList api={recommendedAnime}/>
 
       </section>
 
--- src/app/populer/page.jsx
+++ src/app/populer/page.jsx
@@ -4,13 +4,13 @@ import React, { useEffect, useState } from "react";
 import HeaderMenu from "@/components/Utilities/HeaderMenu";
 import Pagination from "@/components/Utilities/Pagination";
 import AnimeList from "@/components/AnimeList";
-import { getAnimeResponse } from "../libs/api-libs";
+import { getAnimeResponse } from "../../libs/api-libs";
 
 const Page = () => {
     const [page, setPage] = useState(1)
     const [topAnime, setTopAnime] = useState([])
 
-    const fetchData = async() => {
+    const fetchData = async () => {
 
         const populerAnime = await getAnimeResponse("top/anime", `page=${page}`)
             setTopAnime(populerAnime)
